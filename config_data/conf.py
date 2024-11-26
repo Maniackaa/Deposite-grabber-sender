b3ae2fc6d740fcbf6cb497c93440d83733f77c07
Maniackaa <maniac_kaa@mail.ru>
2024-11-26 17:10:43 +0700

    last ver

--- config_data/conf.py
+++ config_data/conf.py
@@ -59,55 +59,23 @@ LOGGING_CONFIG = {
 }
 
 
-# @dataclass
-# class Ftp:
-#     SCREEN_FOLDER: str
-#     FTP_HOST: str
-#     FTP_PORT: int
-#     FTP_USER: str
-#     FTP_PASSWD: str
-#     FTP_TIMEOUT: float
-#     WORKER: str
-#     ENDPOINT: str
-
-
 @dataclass
 class ADB:
     SCREEN_FOLDER: str
     WORKER: str
-    M10_ENDPOINT: str
-    ATB_ENDPOINT: str
-    OCR_ENDPOINT: str
-    M10_PHONES: list
-    ATB_PHONES: list
-    ATB_NAMES: list
+    ENDPOINT: str
 
 
 @dataclass
 class Config:
-    # ftp: Ftp
     adb: ADB
 
 
 def load_config(path=None) -> Config:
     return Config(
-        # ftp=Ftp(SCREEN_FOLDER=os.getenv('SCREEN_FOLDER'),
-        #                   FTP_HOST=os.getenv('FTP_HOST'),
-        #                   FTP_PORT=int(os.getenv('FTP_PORT')),
-        #                   FTP_USER=os.getenv('FTP_USER'),
-        #                   FTP_PASSWD=os.getenv('FTP_PASSWD'),
-        #                   FTP_TIMEOUT=float(os.getenv('FTP_TIMEOUT')),
-        #                   WORKER=os.getenv('WORKER'),
-        #                   ENDPOINT=os.getenv('ENDPOINT'),
-        #                   ),
         adb=ADB(SCREEN_FOLDER=os.getenv('SCREEN_FOLDER'),
                 WORKER=os.getenv('WORKER'),
-                M10_ENDPOINT=os.getenv('M10_ENDPOINT'),
-                ATB_ENDPOINT=os.getenv('ATB_ENDPOINT'),
-                OCR_ENDPOINT=os.getenv('OCR_ENDPOINT'),
-                M10_PHONES=os.getenv('M10_PHONES').split(','),
-                ATB_PHONES=os.getenv('ATB_PHONES').split(','),
-                ATB_NAMES=os.getenv('ATB_NAMES').split(','),
+                ENDPOINT=os.getenv('ENDPOINT'),
                 ),
     )
 
@@ -115,7 +83,6 @@ def load_config(path=None) -> Config:
 load_dotenv()
 
 conf = load_config()
-print(conf)
 
 def get_my_loggers():
     import logging.config
